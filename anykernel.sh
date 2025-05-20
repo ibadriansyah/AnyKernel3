81ca14b923e8efd471c6da184ba3cc762505897e
Dabskutz <71469848+Ibadriansyah@users.noreply.github.com>
2025-05-20 13:15:57 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Salsa-Kernel by: Dabskutz 
+kernel.string=Auldlin-Kernel-5.4 
 do.devicecheck=1
 do.modules=1
 do.systemless=1
